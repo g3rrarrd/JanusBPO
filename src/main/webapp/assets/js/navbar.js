26eb0142002c5204941636c2999f8799fcf6cbcd
Alejandra Solano <alejandra133valle@gmail.com>
2024-08-21 07:46:20 -0600

    XXX

---
+++ src/main/webapp/assets/js/navbar.js
@@ -0,0 +1,39 @@
+/**
+ * Se encargara de manejar los botones de la barra de navegacion
+ */
+
+function clickEmp(){
+	
+}
+
+function clickDepa(){
+	
+}
+
+function clickPlant(){
+	
+}
+
+function clickEva(){
+	
+}
+
+function clickPerm(){
+	
+}
+
+function clickIngresarEmp(){
+	
+}
+
+function clickIngresarNom(){
+	
+}
+
+function clickIngresarPer(){
+	
+}
+
+function clickAsis(){
+	
+}
